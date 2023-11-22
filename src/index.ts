5bd77b791a38ed974772352b3f2c0fa88e024729
frostime <frostime@foxmail.com>
2023-11-22 15:50:38 +0800

    :sparkles: feat(setting): 配置日期的模板

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@
  * @Author       : frostime
  * @Date         : 2023-08-19 18:51:23
  * @FilePath     : /src/index.ts
- * @LastEditTime : 2023-11-22 14:42:45
+ * @LastEditTime : 2023-11-22 15:46:33
  * @Description  : 
  */
 import {
     Plugin,
-    Protyle
+    Protyle,
+    Setting
 } from "siyuan";
+
+import "./index.scss";
+import { title } from "process";
 
 
 const renderString = (template: string, data: { [key: string]: string }) => {
@@ -67,6 +71,62 @@ export default class InsertTimePlugin extends Plugin {
 
         await this.load();
         this.updateSlash();
+
+        this.setting = new Setting({
+            width: '700px',
+            height: '500px',
+            destroyCallback: () => { },
+            confirmCallback: () => {
+                const config_items: NodeListOf<HTMLElement> = document.querySelectorAll('.plugin-insert-time__conf-item');
+                for (let ele of config_items) {
+                    const key = ele.dataset.key;
+                    const name = ele.dataset.name;
+                    const template = (<HTMLInputElement>ele.querySelector('#template')).value;
+                    const filter = (<HTMLInputElement>ele.querySelector('#filter')).value.split(',').map((item) => item.trim());
+                    this.Templates[key] = { name, template, filter };
+                    console.debug(key, name, template, filter);
+                }
+                this.updateSlash();
+                this.saveData('templates.json', this.Templates);
+            }
+        });
+        this.setting.addItem({
+            title: '<span style="color: var(--b3-theme-primary); font-size: 1.25em; font-weight: bold">Hint</span>',
+            description: `插入模板: 时间的格式化模板, 如 yyyy-MM-dd HH:mm:ss 表示 2020-01-01 12:00:00<br>输入指令: / 后面跟的指令, 多种指令使用 , 号分隔`
+        })
+        for (let key in this.Templates) {
+            const Templates = this.Templates;
+            this.setting.addItem({
+                title: Templates[key].name,
+                createActionElement() {
+                    let template = Templates[key];
+                    const html = `
+                    <div class="fn__flex-column plugin-insert-time__conf-item" data-key="${key}" data-name="${template.name}">
+                        <div class="form-row">
+                            <span display="inline-block">插入模板</span>
+                            <div class="fn__space"></div>
+                            <input
+                                class="b3-text-field fn__flex-center fn__size200"
+                                type="text" id="template" value="${template.template}"
+                            />
+                        </div>
+                        <div class="form-row">
+                            <span display="inline-block">输入指令</span>
+                            <div class="fn__space"></div>
+                            <input
+                                class="b3-text-field fn__flex-center fn__size200"
+                                type="text" id="filter" value="${template.filter.join(',')}"
+                            />
+                        </div>
+                    </div>
+                    `;
+                    const div = document.createElement('div');
+                    div.innerHTML = html;
+                    return div;
+                }
+            });
+        }
+
         window.addEventListener('keypress', this.updateBindThis);
     }
 
